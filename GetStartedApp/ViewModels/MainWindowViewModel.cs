19c188e6fccea5b2fb4d67b79f3c1e0ec160d53e
Paraross <Adrago@onet.pl>
2025-04-24 19:56:22 +0200

    Create Avalonia project prototype

---
+++ GetStartedApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace GetStartedApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private const string DEFAULT_GREETING = "Hello World from Avalonia.Samples";
+
+    public MainWindowViewModel()
+    {
+        ButtonClickedCommand = new RelayCommand(ButtonClicked);
+    }
+
+    public string Name
+    {
+        get;
+        set
+        {
+            SetProperty(ref field, value);
+            Greeting = value;
+        }
+    } = "";
+
+    public string Greeting
+    {
+        get;
+        set
+        {
+            SetProperty(ref field, value == "" ? DEFAULT_GREETING : $"Hello {value}");
+        }
+    } = DEFAULT_GREETING;
+
+    public ObservableCollection<string> Lines { get; } = [];
+
+    public ICommand ButtonClickedCommand { get; }
+
+    private void ButtonClicked()
+    {
+        Lines.Add(Greeting);
+    }
+}
---
+++ GetStartedApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace GetStartedApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
