db68129db1246c88939ad97ab740de040bba2aaa
Paraross <Adrago@onet.pl>
2025-05-11 20:38:02 +0200

    Implement test login functionality

--- GetStartedApp/ViewModels/MainWindowViewModel.cs
+++ GetStartedApp/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using GetStartedApp.Models;
@@ -8,61 +11,49 @@ namespace GetStartedApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private const string DEFAULT_GREETING = "Hello World from Avalonia.Samples";
-
     public string Username
     {
         get => field;
         set => SetProperty(ref field, value);
-    }
+    } = "";
+
     public string Password
     {
         get => field;
         set => SetProperty(ref field, value);
-    }
-    public MainWindowViewModel()
-    {
-        ButtonClickedCommand = new RelayCommand(ButtonClicked);
-    }
-
-    public string Name
-    {
-        get;
-        set
-        {
-            SetProperty(ref field, value);
-            Greeting = value;
-        }
     } = "";
 
-    public string Greeting
+    public MainWindowViewModel()
     {
-        get;
-        set
-        {
-            SetProperty(ref field, value == "" ? DEFAULT_GREETING : $"Hello {value}");
-        }
-    } = DEFAULT_GREETING;
+        LoginButtonClickedCommand = new RelayCommand(LoginButtonClicked);
+    }
 
     public ObservableCollection<string> Lines { get; } = [];
 
-    public ICommand ButtonClickedCommand { get; }
+    public ICommand LoginButtonClickedCommand { get; }
 
-    private void ButtonClicked()
+    private void LoginButtonClicked()
     {
-        // WARN: crashuje przy pustym lub nieunikatowym inpucie
-        using (var context = new TimeTrackingContext())
+        using var context = new TimeTrackingContext();
+        var user = new User
         {
-            var newUser = new User
-            {
-                Name = $"{Name}",
-                Password = $"{Greeting}",
-                Type = "admin",
-            };
+            Name = $"{Username}",
+            Password = $"{Password}",
+            Type = "admin",
+        };
 
-            context.Users.Add(newUser);
-            context.SaveChanges();
+        var matchingUsers =
+            from u in context.Users
+            where u.Name == user.Name && u.Password == user.Password
+            select u;
+
+        if (matchingUsers.Count() == 1)
+        {
+            Console.WriteLine("Logged in");
         }
-        Lines.Add(Greeting);
+        else
+        {
+            Console.WriteLine("Incorrect email or password");
+        }
     }
 }
