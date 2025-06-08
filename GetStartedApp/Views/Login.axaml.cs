f2b30cfe8da669dc7b554165d89a9f18c6921eb9
Paraross <Adrago@onet.pl>
2025-06-08 18:18:53 +0200

    Improve login UX Email textbox is focused by default Pressing enter on password textbox logs in

--- GetStartedApp/Views/Login.axaml.cs
+++ GetStartedApp/Views/Login.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +9,21 @@ public partial class LoginView : UserControl
     public LoginView()
     {
         InitializeComponent();
+        Loaded += FocusEmailTextBox;
+    }
+
+    private void FocusEmailTextBox(object? sender, RoutedEventArgs e)
+    {
+        EmailTextBox.Focus();
+    }
+
+    private void PasswordTextBoxKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter)
+        {
+            return;
+        }
+
+        LoginButton.Command!.Execute(null);
     }
 }
