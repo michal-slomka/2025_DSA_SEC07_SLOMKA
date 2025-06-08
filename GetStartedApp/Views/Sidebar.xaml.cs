165907a8dbe53794aa62a7c221c645a313fa088b
WiktorChrobak <wiktor.chrobak@poczta.onet.pl>
2025-06-08 17:01:11 +0200

    Create Sidebar file

---
+++ GetStartedApp/Views/Sidebar.xaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GetStartedApp.Views
+{
+    public partial class Sidebar : UserControl
+    {
+        public Sidebar()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
